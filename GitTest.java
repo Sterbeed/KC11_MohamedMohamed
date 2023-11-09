6da10d8782aa087d1a3ce04f69c2fb20e3dca709
Sterbeed <110668869+Sterbeed@users.noreply.github.com>
2023-11-09 09:50:03 -0500

    A simple test project that prints Hello World and a 1-100 loop.

---
+++ GitTest.java
@@ -0,0 +1,15 @@
+public class GitTest
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Hello world.");
+
+        for(int i=1; i < 101; i++)
+        {
+            System.out.println(i);
+        }
+    }
+
+
+
+}
--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # KC11_MohamedMohamed
- 
+Testing Git and Github for CIS 111B
